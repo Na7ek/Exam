f4518dfbe61444f0cd4df81cd1e61cfc746297a0
Na7ek <khatayannn007@gmail.com>
2026-01-19 12:34:51 +0400

    "Fixed footer!!!"

--- Exam/src/Components/Footer.jsx
+++ Exam/src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>© 2026 NK/MK. All rights reserved.</p>
+      <p>© 2026 MK/NK. All rights reserved.</p>
 
       <div className="creators">
         <a
